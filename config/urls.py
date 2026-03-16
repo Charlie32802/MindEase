f4b2415c055ec4e96efcecbcb5fc2f73a484eec0
Marc Daryll Trinidad <marcdaryll.trinidad@gmail.com>
2026-03-16 18:32:27 +0800

    Enhance UI: Add cinematic 3D bubbles, liquid morphism forms, and fix Daphne static routing

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from admin_panel.admin import custom_admin_site
 
 urlpatterns = [
@@ -20,3 +21,9 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # Fallback to serve static/media files through Daphne when DEBUG=False (Local Production)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
